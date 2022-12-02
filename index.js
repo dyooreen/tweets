e4db54a6b701cc880b6281c240028df2980e946e
lofigrammer <lofigrammer@gmail.com>
2022-12-03 02:27:55 +0400

    Remove Github Actions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const twit = require("./twitter");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-const port = process.env.PORT || 3000;
+const port = ;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
